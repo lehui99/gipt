20585ab7572d06fff6b403c7a40a142820b2b804
lehui99 <lehui99@gmail.com>
2014-06-14 19:23:13 +0800

    finish gipt.py

--- gipt.py
+++ gipt.py
@@ -2,42 +2,129 @@ import json
 import socket
 from threading import Thread
 import time
+import random
 import sys
 import socks
 
 class ProxySocket(object):
 
     def __init__(self, config, socksProxy):
+        self.config = config
         if type(socksProxy) == int:
             self.host = config['defaultSocksHost']
             self.port = socksProxy
         else:
             self.host = socksProxy[0]
             self.port = socksProxy[1]
+        self.failCount = 0
+        self.fail = False
 
     def apply(self, socksocket):
         socksocket.set_proxy(socks.SOCKS5, self.host, self.port)
+        socksocket.settimeout(self.config['socketTimeout'])
 
 class CheckProxies(Thread):
 
     def __init__(self, config):
         Thread.__init__(self)
+        self.daemon = True
         self.config = config
 
     def run(self):
         while True:
             for socksProxy in self.config['socksProxies']:
-                pass
+                s = socks.socksocket()
+                socksProxy.apply(s)
+                try:
+                    s.connect(('www.google.com', 80))
+                    s.send(bytearray(b'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'))
+                    if s.recv(65536) == '':
+                        raise Exception()
+                    socksProxy.fail = False
+                except Exception:
+                    socksProxy.failCount += 1
+                    socksProxy.fail = True
             time.sleep(self.config['checkInterval'])
+
+class ProxySelector(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(self):
+        goodProxies = [socksProxy for socksProxy in self.config['socksProxies'] if not socksProxy.fail]
+        goodProxies.sort(cmp = lambda x, y: x.failCount - y.failCount)
+        return goodProxies[random.randint(0, len(goodProxies)) * random.randint(0, len(goodProxies)) / len(goodProxies) - 1]
+
+class Pipe(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+
+    def setSockPair(self, sockIn, sockOut):
+        self.sockIn = sockIn
+        self.sockOut = sockOut
+
+    def pipeData(self):
+        while True:
+            sockOut.send(sockIn.recv(65536))
+
+    def run(self):
+        self.pipeData()
+
+class Tunnel(Pipe):
+
+    def __init__(self, config, sock, hosts, proxySelector):
+        Pipe.__init__(self)
+        self.config = config
+        self.sock = sock
+        self.hosts = hosts
+        self.proxySelector = proxySelector
+
+    def run(self):
+        proxySock = socks.socksocket()
+        self.proxySelector().apply(proxySock)
+        host = self.hosts[random.randint(0, len(self.hosts) - 1)]
+        proxySock.connect((host[0], host[1]))
+        pipe = Pipe()
+        pipe.setSockPair(proxySock, self.sock)
+        pipe.start()
+        self.setSockPair(self.sock, proxySock)
+        self.pipeData()
+
+class Server(Thread):
+
+    def __init__(self, config, port, hosts, proxySelector):
+        Thread.__init__(self)
+        self.config = config
+        self.hosts = hosts
+        self.proxySelector = proxySelector
+        self.sock = socket.socket()
+        self.sock.bind(('0.0.0.0', port))
+        self.sock.listen()
+
+    def run(self):
+        #tunnels = []
+        while True:
+            clientSock = self.sock.accept()
+            tunnel = Tunnel(self.config, clientSock, self.hosts, self.proxySelector)
+            tunnel.start()
+            #tunnels.append(pipe)
 
 class Main(object):
 
     def __init__(self, config):
-        self.config = config
         config['socksProxies'] = [ProxySocket(config, socksProxy) for socksProxy in config['socksProxies']]
+        self.config = config
 
     def start(self):
         CheckProxies(self.config).start()
+        #servers = []
+        proxySelector = ProxySelector()
+        for k, v in self.config['tunnelServers'].iteritems():
+            server = Server(self.config, int(k), v, proxySelector)
+            server.start()
+            #servers.append(tunnel)
 
 if __name__ == '__main__':
     Main(json.loads(open(sys.argv[1], 'r').read())).start()
